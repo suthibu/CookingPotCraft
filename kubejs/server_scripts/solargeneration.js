14baf4f0c5bdc025ad1be16a5ca15864f24d01be
スティーブ <suthibu28@gmail.com>
2025-01-27 23:02:10 +0900

    SolarGenerationのレシピを追加

---
+++ kubejs/server_scripts/solargeneration.js
@@ -0,0 +1,74 @@
+ServerEvents.recipes(event =>{
+event.shaped( //hardened solar core
+    Item.of("solargeneration:solar_core_hardened"),
+    [
+        " A ",
+        "ABA",
+        " A "
+    ],
+    {
+        A:"solarflux:photovoltaic_cell_1",
+        B:"solarflux:sp_3"
+    }
+)
+event.shaped( //redstyone solar core
+    Item.of("solargeneration:solar_core_redstone"),
+    [
+        " A ",
+        "ABA",
+        " A "
+    ],
+    {
+        A:"solarflux:photovoltaic_cell_2",
+        B:"solarflux:sp_4"
+    }
+)
+event.shaped( //signalum solar core
+    Item.of("solargeneration:solar_core_signalum"),
+    [
+        " A ",
+        "ABA",
+        " A "
+    ],
+    {
+        A:"solarflux:photovoltaic_cell_3",
+        B:"solarflux:sp_5"
+    }
+)
+event.shaped( //resonant solar core
+    Item.of("solargeneration:solar_core_resonant"),
+    [
+        " A ",
+        "ABA",
+        " A "
+    ],
+    {
+        A:"solarflux:photovoltaic_cell_4",
+        B:"solarflux:sp_6"
+    }
+)
+event.shaped( //advanced solar core
+    Item.of("solargeneration:solar_core_advanced"),
+    [
+        " A ",
+        "ABA",
+        " A "
+    ],
+    {
+        A:"solarflux:photovoltaic_cell_5",
+        B:"solarflux:sp_7"
+    }
+)
+event.shaped( //ultimate solar core
+    Item.of("solargeneration:solar_core_ultimate"),
+    [
+        " A ",
+        "ABA",
+        " A "
+    ],
+    {
+        A:"solarflux:photovoltaic_cell_6",
+        B:"solarflux:sp_8"
+    }
+)
+})
