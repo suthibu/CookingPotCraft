14acfebf864b499ed89d9d231a8a92ec1df81d3d
スティーブ <suthibu28@gmail.com>
2025-02-06 23:01:18 +0900

    お鍋鍛冶型

--- kubejs/server_scripts/vanila.js
+++ kubejs/server_scripts/vanila.js
@@ -34,3 +34,49 @@ ServerEvents.recipes(event => {
         }
       })  
 })
+ServerEvents.recipes(event => {
+  /** 
+   * FarmersDelightのポットでレシピを追加する
+   * @param {String} output           完成品
+   * @param {String[]} inputs         必要なアイテム
+   * @param {String?} containerItem    容器
+   */
+  let potRecipe = (output, inputs, containerItem) => {
+      event.remove({ output: output })
+
+      if (containerItem) {
+          event.custom({
+              "type": "farmersdelight:cooking",
+              "container": {
+                  "count": 1,
+                  "id": containerItem
+              },
+              "cookingtime": 0,
+              "experience": 1.0,
+              "ingredients": inputs.map(item => {
+                  return { "item": item }
+              }
+              ),
+              "recipe_book_tab": "meals",
+              "result": {
+                  "id": output
+              }
+          })
+      } else {
+          event.custom({
+              "type": "farmersdelight:cooking",
+              "cookingtime": 0,
+              "experience": 1.0,
+              "ingredients": inputs.map(item => {
+                  return { "item": item }
+              }
+              ),
+              "recipe_book_tab": "meals",
+              "result": {
+                  "id": output
+              }
+          })
+      }
+  }
+potRecipe("minecraft:netherite_upgrade_smithing_template",["minecraft:diamond","minecraft:diamond","minecraft:diamond","minecraft:netherrack","minecraft:netherrack","minecraft:netherrack"],"minecraft:netherite_ingot") //ネザライト強化の鍛治型
+})
